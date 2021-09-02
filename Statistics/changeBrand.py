b95626c3077a959294c0993f54afa9855d160d7e
fixmeseb <43381771+fixmeseb@users.noreply.github.com>
2021-09-02 15:36:55 -0400

    Just clearing Abe out of the way in case I need to roll back some stuff.

--- Statistics/changeBrand.py
+++ Statistics/changeBrand.py
@@ -3,10 +3,11 @@
 alphabet = ['#', "A", "B", 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 import os
-brandsFile = open("Statistics\\brands.txt", "w",encoding='utf8')
-completedCharacters = []
-brandsList = []
-brandsDict = {}
+
+chooseFranchise = input("Enter franchise (the second level): ")
+chooseBrand = input("Enter brand: ")
+
+
 # List all files in a directory using os.listdir
 for letter in alphabet:
     basepath = 'Characters/' + letter
@@ -15,4 +16,20 @@ for letter in alphabet:
             characterFile = open(basepath + "/" + entry, "r", encoding='utf8')
             characterInfo = characterFile.read().split("\n")
             if len(characterInfo) == 29:
-                if characterInfo[]
+                if characterInfo[21] == chooseFranchise:
+                    firstLetter = entry[0:1:]
+                    if firstLetter in numbers:
+                        firstLetter = "#"
+                    characterFile = open("Characters\\" + firstLetter + "\\" + character + ".txt", "w", encoding='utf8')
+                    print("Found " + characterInfo[0])
+                    newAssembly = characterInfo[0]
+                    for characterInfoTag in range(1,25):
+                        newAssembly = newAssembly + "\n" + characterInfo[characterInfoTag]
+                    #newAssembly = newAssembly + "\n" + "COLOR"
+                    #print("[\n" + newAssembly + "\n]")
+                    brand = chooseBrand
+                    deuxAssembly = ""
+                    for characterInfoTag in range(26,29):
+                        deuxAssembly = deuxAssembly + "\n" + characterInfo[characterInfoTag]
+                    
+                    characterFile.write(newAssembly + "\n" + brand + deuxAssembly)
